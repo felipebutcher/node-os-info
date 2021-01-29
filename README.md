4d5951ddaabc1ac94007dc60a0163dbde5c61325
Felipe Butcher <felipebutcher@gmail.com>
2021-01-28 22:45:53 -0300

    cpu percentage

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 When I needed to get memory, cpu and disk information on NodeJS, I ended up looking for answers on stackoverflow and using multiple packages with some code. I am putting all together here
+
+
+copied code from here:
+https://gist.github.com/GaetanoPiazzolla/c40e1ebb9f709d091208e89baf9f4e00
--- index.js
+++ index.js
@@ -1,10 +1,59 @@
+const os = require("os");
 
-
-exports.cpuPercentAverage = (seconds) => {
-
+function cpuAverage() {
+    var totalIdle = 0, totalTick = 0;
+    var cpus = os.cpus();
+    for (var i = 0, len = cpus.length; i < len; i++) {
+        var cpu = cpus[i];
+        for (type in cpu.times) {
+            totalTick += cpu.times[type];
+        }
+        totalIdle += cpu.times.idle;
+    }
+    return { idle: totalIdle / cpus.length, total: totalTick / cpus.length };
 }
 
-exports.cpuPercent = () => {
-    return "Hello World my son of a biatch";
+const arrAvg = function (arr) {
+    if (arr && arr.length >= 1) {
+        const sumArr = arr.reduce((a, b) => a + b, 0)
+        return sumArr / arr.length;
+    }
+};
+
+function getCPULoadAVG(avgTime = 1000, delay = 100) {
+    return new Promise((resolve, reject) => {
+        const n = ~~(avgTime / delay);
+        if (n <= 1) {
+            reject('Error: interval to small');
+        }
+        let i = 0;
+        let samples = [];
+        const avg1 = cpuAverage();
+        let interval = setInterval(() => {
+            if (i >= n) {
+                clearInterval(interval);
+                resolve(~~((arrAvg(samples) * 100)));
+            }
+            const avg2 = cpuAverage();
+            const totalDiff = avg2.total - avg1.total;
+            const idleDiff = avg2.idle - avg1.idle;
+            samples[i] = (1 - idleDiff / totalDiff);
+            i++;
+        }, delay);
+    });
 }
+
+exports.cpuPercent = callback => {
+    getCPULoadAVG(1000, 100).then((avg) => {
+        callback(avg);
+    });
+}
+
+exports.cpuPercentAverage = (seconds, callback) => {
+    getCPULoadAVG(seconds * 1000, 100).then((avg) => {
+        callback(avg);
+    });
+}
+
+
 
